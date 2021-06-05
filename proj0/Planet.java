53503177f260cecf5b752f078ff5ae2cbacf6af2
mefs <dennis.mefs@gmail.com>
2021-06-05 19:20:19 +0800

    proj0: Make all variables public

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,7 +1,7 @@
 public class Planet {
-    double xxPos, yyPos, xxVel, yyVel, mass;
-    String imgFileName;
-    static final double gravity = 6.67e-11;
+    public double xxPos, yyPos, xxVel, yyVel, mass;
+    public String imgFileName;
+    private static final double gravity = 6.67e-11;
 
     public Planet(double xP, double yP, double xV, double yV, double m, String img) {
         xxPos = xP;
