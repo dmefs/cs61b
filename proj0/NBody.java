bd5936d2a3545b5f0d8aa41c0ad6f80913a569ac
mefs <dennis.mefs@gmail.com>
2021-06-05 18:26:35 +0800

    proj0: Implement ReadPlanets

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -5,4 +5,17 @@ public class NBody {
         in.readInt();
         return in.readDouble();
     }
+
+    public static Planet[] readPlanets(String s) {
+        In in = new In(s);
+        int num = in.readInt();
+        double radius = in.readDouble();
+        Planet[] ps = new Planet[num];
+
+        for (int i = 0; i < num; i++) {
+            ps[i] = new Planet(in.readDouble(), in.readDouble(), in.readDouble(), in.readDouble(), in.readDouble(),
+                    in.readString());
+        }
+        return ps;
+    }
 }
