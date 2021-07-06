3222ab4f2bdfa13e737180e62823a8c2135ce8df
css438_dennis <dennis.mefs@gmail.com>
2021-07-07 02:29:53 +0800

    proj1b: Implement OffByOne

---
+++ proj1b/OffByOne.java
@@ -0,0 +1,6 @@
+public class OffByOne implements CharacterComparator{
+    public boolean equalChars(char x, char y) {
+        int result = x - y;
+        return (result == 1) || (result == -1);
+    }
+}
