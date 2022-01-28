b6853be020e39cc1bdbd197565c4d86846fcce86
css438_dennis <dennis.mefs@gmail.com>
2022-01-28 16:49:16 +0800

    hw1: throw exception when peek() is empty

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -79,7 +79,7 @@ public class ArrayRingBuffer<T>  extends AbstractBoundedQueue<T> {
      */
     public T peek() {
         if (isEmpty()) {
-            return null;
+            throw new RuntimeException("Ring Buffer Underflow");
         }
         return rb[first];
     }
